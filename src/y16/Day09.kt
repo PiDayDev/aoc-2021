41a0e7efbda6246225ba8593e512acfeac2784b0
Damiano Salvi <salvi@intre.it>
2021-12-15 18:03:30 +0100

    ✨ 2016 🎅 Day 9 / part 2

--- src/y16/Day09.kt
+++ src/y16/Day09.kt
@@ -3,13 +3,13 @@ package y16
 private const val day = "09"
 
 fun main() {
-    fun String.decompress(): StringBuilder {
+    fun String.decompress(protocolVersion: Int = 1): Long {
         var inMarker = false
         var marker = ""
         var markLen = 0
         var markRep = 0
         var data = ""
-        val buffer = StringBuilder()
+        var len = 0L
         for (c in this) {
             if (inMarker) {
                 if (c == ')') {
@@ -26,30 +26,35 @@ fun main() {
                     inMarker = true
                     marker = ""
                 } else {
-                    buffer.append(c)
+                    len++
                 }
             } else if (data.length < markLen) {
                 data += c
                 if (data.length == markLen) {
-                    buffer.append(data.repeat(markRep))
+                    len += markRep * when (protocolVersion) {
+                        2 -> data.decompress(2)
+                        else -> data.length.toLong()
+                    }
                     data = ""
                     markRep = 0
                     markLen = 0
                 }
             } else {
-                buffer.append(c)
+                len++
             }
         }
-        return buffer
+        return len
     }
 
-    fun String.decompressedSize() = decompress().length.toLong()
+    fun part1(input: String) = input.decompress()
 
-    fun part1(input: String) = input.decompressedSize()
+    fun part2(input: String) = input.decompress(2)
 
-    fun part2(input: String): Int {
-        return input.length
-    }
+    check("X(8x2)(3x3)ABCY".decompress() == "X(3x3)ABC(3x3)ABCY".length.toLong())
+    check("X(8x2)(3x3)ABCY".decompress(2) == "XABCABCABCABCABCABCY".length.toLong())
+    check("(6x1)(1x3)A".decompress() == "(1x3)A".length.toLong())
+    check("(6x1)(1x3)A".decompress(2) == "AAA".length.toLong())
+    check("(27x12)(20x12)(13x14)(7x10)(1x12)A".decompress(2) == 241_920L)
 
     val input = readInput("Day${day}").joinToString("").replace("""\s""".toRegex(), "")
     println(part1(input))
