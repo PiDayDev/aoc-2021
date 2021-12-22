332f024e1f0cf66a33965a5103998d32d20f6e8d
Damiano Salvi <salvi@intre.it>
2021-12-22 16:50:04 +0100

    ♻ 2021 🎅 Day 21

--- src/Day21.kt
+++ src/Day21.kt
@@ -3,28 +3,51 @@ import kotlin.math.min
 
 private const val day = "21"
 
+private interface Game {
+    fun isOver(): Boolean
+    fun player1(): Game
+    fun player2(): Game
+    fun play(): Game {
+        println(this)
+
+        val seq = generateSequence(this) {
+            val g1 = it.player1()
+            val g2 = g1.player2()
+            if (g1.isOver()) g1 else g2
+        }
+
+        val end = seq.dropWhile { !it.isOver() }.first()
+
+        println(end)
+        return end
+    }
+}
+
 fun main() {
-    data class Game(
+
+    data class DeterministicGame(
         val p1: Int, val p2: Int,
         val score1: Int = 0, val score2: Int = 0,
         private val dice: Iterator<Int>, val rolls: Int = 0
-    ) {
+    ) : Game {
         fun increase(n: Int) = when (val v = (n + dice.next() + dice.next() + dice.next()) % 10) {
             0 -> 10
             else -> v
         }
 
-        fun player1(): Game {
+        override fun player1(): DeterministicGame {
             val dest = increase(p1)
             return copy(p1 = dest, score1 = score1 + dest, rolls = rolls + 3)
         }
 
-        fun player2(): Game {
+        override fun player2(): DeterministicGame {
             val dest = increase(p2)
             return copy(p2 = dest, score2 = score2 + dest, rolls = rolls + 3)
         }
 
-        fun isOver() = score1 >= 1000 || score2 >= 1000
+        override fun isOver() = score1 >= 1000 || score2 >= 1000
+
+        override fun play() = super.play() as DeterministicGame
 
         override fun toString() = "" +
                 "| p1: $p1".padEnd(10) +
@@ -35,22 +58,8 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        val dice: Iterator<Int> = generateSequence(1) { (it % 100) + 1 }.iterator()
         val (p1, p2) = input.map { it.substringAfter(": ").toInt() }
-        val start = Game(p1 = p1, p2 = p2, dice = dice)
-
-        println(start)
-
-        val end = generateSequence(start) {
-            val g1 = it.player1()
-            val g2 = g1.player2()
-            if (g1.isOver()) g1 else g2
-        }
-            .dropWhile { !it.isOver() }
-            .first()
-
-        println(end)
-
+        val end = DeterministicGame(p1 = p1, p2 = p2, dice = generateSequence(1) { (it % 100) + 1 }.iterator()).play()
         return min(end.score1, end.score2) * end.rolls
     }
 
@@ -96,54 +105,38 @@ fun main() {
         val states: List<DiracState> = emptyList(),
         val win1: Long = 0L,
         val win2: Long = 0L
-    ) {
+    ) : Game {
         constructor(start1: Int, start2: Int) : this(
             states = listOf(DiracState(position1 = start1, position2 = start2))
         )
 
-        fun player1(): Dirac {
+        override fun player1(): Dirac {
             val newP1 = states.flatMap { it.player1() }
             val (wins, rest) = newP1.partition { it.score1 >= 21 }
             return copy(states = rest, win1 = win1 + wins.sumOf { it.universes })
         }
 
-        fun player2(): Dirac {
+        override fun player2(): Dirac {
             val newP2 = states.flatMap { it.player2() }
             val (wins, rest) = newP2.partition { it.score2 >= 21 }
             return copy(states = rest, win2 = win2 + wins.sumOf { it.universes })
         }
 
-        fun isOver() = states.isEmpty()
+        override fun isOver() = states.isEmpty()
 
-        override fun toString() = """
-            Dirac (Wins: $win1 VS $win2)
-            ${states.size}
-        """.trimIndent()
+        override fun play() = super.play() as Dirac
+
+        override fun toString() = "Dirac Wins: $win1 VS $win2"
     }
 
 
     fun part2(input: List<String>): Long {
         val (p1, p2) = input.map { it.substringAfter(": ").toInt() }
-        val start = Dirac(p1, p2)
-
-        println(start)
-
-        val end = generateSequence(start) {
-            val turn1 = it.player1()
-            val turn2 = turn1.player2()
-            if (turn1.isOver()) turn1 else turn2
-        }
-            .dropWhile { !it.isOver() }
-            .first()
-
-        println(end)
-
+        val end = Dirac(p1, p2).play()
         return max(end.win1, end.win2)
     }
 
     val input = readInput("Day${day}")
     println(part1(input))
-    val ans2 = part2(input)
-    check(ans2 > 137295809L)
-    println(ans2)
+    println(part2(input))
 }
