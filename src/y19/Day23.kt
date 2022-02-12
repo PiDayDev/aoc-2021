277447ae4bf58fffeb68946b3a6f3694109ffe19
Damiano Salvi <salvi@intre.it>
2022-02-12 20:17:50 +0100

    ✨ 2019 🎅 Day 23 / Part 2

--- src/y19/Day23.kt
+++ src/y19/Day23.kt
@@ -2,54 +2,80 @@ package y19
 
 import kotlinx.coroutines.*
 import java.util.*
+import java.util.concurrent.atomic.AtomicLong
 
 private const val day = 23
 
+private const val NAT = 255L
+
 @ExperimentalCoroutinesApi
 fun main() {
+    fun List<Long>.repeatedY(): Long? = this
+        .filterIndexed { index, _ -> index % 2 == 0 }
+        .zipWithNext()
+        .firstOrNull { it.first == it.second && it.first >= 0 }
+        ?.first
 
     fun simulateNetwork(
         codes: List<Long>,
         stopCondition: (List<Long>) -> Boolean
-    ): MutableList<Long> {
-        val ids = 0L..49L
-        val channels = ids.associateWith { Stack<Long>().apply { push(it) } }
+    ): Long {
+        val ids = (0L..49L).toList()
+        val channels = ids.associateWith { id -> LinkedList<Long>().apply { push(id) } } + (NAT to LinkedList<Long>())
+        val processes = ids.associateWith { id -> AsyncIntCodeProcessor(codes, id) }
 
-        val result = mutableListOf<Long>()
+        val result = AtomicLong(0)
+        val reports = mutableListOf<Long>()
         val jobs = mutableListOf<Job>()
 
         runBlocking {
             ids.forEach { id ->
-                val process = IntCodeProcessor23(codes, id)
                 jobs += launch {
-                    result += process.asyncExecute(channels, stopCondition)
+                    val process = processes[id]!!
+                    result.set(process.asyncExecute(channels, stopCondition).first())
                     jobs.forEach { it.cancel("DONE") }
                 }
             }
+            jobs += launch {
+                // NAT
+                val ch0 = channels[0]!!
+                val chNat = channels[NAT]!!
+                while (true) {
+                    if (ids.all { channels[it]!!.isEmpty() } && processes.values.all { it.failureCount > 1 } && chNat.size >= 2) {
+                        val (x, y) = chNat.takeLast(2)
+                        reports += y
+                        reports.repeatedY()?.let { repeatedY ->
+                            jobs.forEach { it.cancel("DONE") }
+                            result.set(repeatedY)
+                        }
+                        ch0 += x
+                        ch0 += y
+                    }
+                    yield()
+                }
+            }
         }
-        return result
+        return result.get()
     }
 
-    fun part1(codes: List<Long>) = simulateNetwork(codes) { it.isNotEmpty() }.first()
+    fun part1(codes: List<Long>) = simulateNetwork(codes) { it.isNotEmpty() }
 
-    fun part2(codes: List<Long>): Long {
-        val stopCondition: (List<Long>) -> Boolean = { it.size >= 100 }
-        val result = simulateNetwork(codes, stopCondition)
-        return result.first()
-    }
+    fun part2(codes: List<Long>) = simulateNetwork(codes) { false }
 
     val input = readInput("Day${day}").codes()
     println(part1(input))
     println(part2(input))
 }
 
-class IntCodeProcessor23(
+class AsyncIntCodeProcessor(
     initialCodes: List<Long>,
     val id: Long
-) : IntCodeProcessor(initialCodes, false) {
+) : IntCodeProcessor(initialCodes) {
+
+    var failureCount = 0
 
     suspend fun asyncExecute(
-        channels: Map<Long, Stack<Long>>,
+        channels: Map<Long, LinkedList<Long>>,
         stopCondition: (List<Long>) -> Boolean
     ): List<Long> {
         val input = channels[id]!!
@@ -57,27 +83,25 @@ class IntCodeProcessor23(
         val result = mutableListOf<Long>()
         while (!halted()) {
             fun inputGetter(): Long {
-                if (input.isEmpty()) runBlocking { yield() }
-                val v = if (input.isEmpty()) -1L else input.pop()
-                println("<=IN=[$id]== $v")
-                return v
+                return if (input.isEmpty()) {
+                    failureCount++
+                    -1L
+                } else {
+                    failureCount = 0
+                    input.pollFirst()
+                }
             }
 
             executeStep(::inputGetter) {
                 buffer.add(it)
-                println("=OUT=[$id]=> ".padEnd(25) + "| " + "| ".repeat(id.toInt()) + "$it  [ buffer size = ${buffer.size} ]")
                 if (buffer.size == 3) {
-                    println("=SEND=> $buffer")
                     val (address, x, y) = buffer
-                    if (address == 255L) {
+                    if (address == NAT) {
                         result += y
-                        println(":" + result.size)
-                        channels[0]!!.push(y)
-                    } else {
-                        val output = channels[address]!!
-                        output.push(y)
-                        output.push(x)
                     }
+                    val output = channels[address]!!
+                    output += x
+                    output += y
                     buffer.clear()
                 }
             }
